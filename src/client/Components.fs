451538909ba3c8733fe1cc8f32ad4a62643f7d52
Joshua Harms <joshua@nozzlegear.com>
2017-12-09 09:49:33 -0600

    Added AfterRender and AfterMount helpers

--- src/client/Components.fs
+++ src/client/Components.fs
@@ -32,3 +32,48 @@ let ControlGroup label control =
         R.label [] [R.str label]
         control
     ]
+
+let TextboxWithLabel label value onChange =
+    ControlGroup label <| R.input [P.Type "text"; P.OnChange (Utils.getValueFromEvent >> onChange); P.Value value]
+
+let PasswordWithLabel label value onChange =
+    ControlGroup label <| R.input [P.Type "password"; P.OnChange (Utils.getValueFromEvent >> onChange); P.Value value]
+
+let DateInputWithLabel label (value: System.DateTime option) onChange =
+    let formattedValue =
+        value
+        |> Option.map (fun d -> d.ToString "YYYY-MM-DD")
+        |> Option.defaultValue ""
+
+    R.input [P.Type "date"; P.OnChange (Utils.getDateFromEvent >> onChange); P.Value formattedValue]
+    |> ControlGroup label
+
+let Checkbox label value onChange =
+    R.div [P.ClassName "control-group checkbox"] [
+        R.label [] [
+            R.input [P.Type "checkbox"; P.OnChange (Utils.getCheckedFromEvent >> onChange); P.Checked value]
+            R.str <| " " + label
+        ]
+    ]
+
+/// Runs the given function after each React render cycle, whether mounting or updating.
+let AfterRender f =
+    let func (el: Browser.Element) =
+        match Option.ofNullable el with
+        | Some _ -> f ()
+        | None -> ()
+
+    R.noscript [P.Ref func] []
+
+/// Runs the given function after the first mount of the parent React component or element.
+/// NOTE: This must be created *outside* of the parent's render cycle, i.e. it should not be created inside a render method or MobxReact.Observer function.
+let AfterMount f =
+    let mutable mounted = false
+    let func el =
+        match Option.ofNullable el with
+        | None -> ()
+        | Some _ ->
+            if not mounted then f()
+            mounted <- true
+
+    R.noscript [P.Ref func] []
