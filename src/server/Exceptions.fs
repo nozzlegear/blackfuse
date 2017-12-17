406015a127de711f4c75b77b8ca36c7c02d6f9a4
Joshua Harms <joshua@nozzlegear.com>
2017-12-17 13:47:56 -0600

    Remove unused Exceptions file

--- src/server/Exceptions.fs
+++
@@ -1,8 +0,0 @@
-module Exceptions
-open Domain
-
-type HttpException (message, status) =
-    inherit System.Exception(message)
-    member x.status: Status.Code = status
-    member x.toErrorResponse () =
-        ErrorResponse.FromStatus message status
