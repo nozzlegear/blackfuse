4447bb865c6365be32d275bd4a69e2e40074de7f
Joshua Harms <joshua@nozzlegear.com>
2018-02-05 10:50:06 -0600

    Remove debug function

--- src/client/stores/Nav.fs
+++ src/client/stores/Nav.fs
@@ -9,8 +9,6 @@ let navIsOpen = mobx.boxedObservable false
 
 let computedMessage = mobx.computed (fun _ -> sprintf "My secret message: %b" <| mobx.get navIsOpen)
 
-let runner = mobx.autorun(fun _ -> printfn "Autorunner running. The navIsOpen value is: %b" <| mobx.get navIsOpen)
-
 let setNavIsOpen toValue = mobx.runInAction(fun _ -> mobx.set navIsOpen toValue)
 
 let toggleNavIsOpen () = mobx.runInAction(fun _ -> mobx.get navIsOpen |> not |> mobx.set navIsOpen)
