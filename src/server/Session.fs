7225183ecad79b9cd1028722112d5284d759e556
Joshua Harms <joshua@nozzlegear.com>
2018-02-09 10:44:30 -0600

    Bugfix: Signing a session would produce different hashes depending on whether the session was parsed from json or created as a record
    In this instance, the user.subscription.price decimal would be 9.00 when stringified in one case, and then 9.0 when stringified in the other case. Fixed this by using a gross hack, stringifying to json, then parsing, then stringifying again before hashing.

--- src/server/Session.fs
+++ src/server/Session.fs
@@ -27,13 +27,20 @@ let private passPhrase =
 
 /// Takes a Session record and signs it with HMACSHA256 and your application's secret key, returning the Session with its `signature` label set to the result.
 let sign (session: Session) =
-    use hasher = new System.Security.Cryptography.HMACSHA256(passPhrase)
+    use hasher = new System.Security.Cryptography.HMACSHA256(System.Text.Encoding.UTF8.GetBytes "henlo_world")
+
+    // This is a gross hack, but I was running into an issue (bug?) where signing a session from the `tryDecode` function below would
+    // produce a different hash than one that was just created as a record. The problem was, when stringified to JSON, the parsed
+    // json's .price label would be 9.00 (two zeroes) and the one created from F# would be 9.0 (one zero). So to hack around that I'm
+    // just stringifying, parsing, then stringifying again to get them to the same "state" and be equal. 
     let hash = 
         session.ToEncodable()
         |> Json.stringify
+        |> Json.parse<Session>
+        |> Json.stringify
         |> Text.Encoding.UTF8.GetBytes
         |> hasher.ComputeHash
-        |> Text.Encoding.UTF8.GetString
+        |> Convert.ToBase64String
 
     { session with signature = hash }
 
@@ -42,9 +49,9 @@ let tryDecode (sessionStr: string) : Session option =
     try
         let session = Json.parse<Session> sessionStr
         let signed = sign session 
-        
+
         if session.signature = signed.signature 
         then Some session
-        else None 
+        else None
     with
     | _ -> None 
