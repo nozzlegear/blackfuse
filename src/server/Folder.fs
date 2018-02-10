e2d8ff961750a481bc4560d6d4ae753a806cb232
Joshua Harms <joshua@nozzlegear.com>
2018-02-10 09:54:24 -0600

    Add more resolution options for the public folder

--- src/server/Folder.fs
+++ src/server/Folder.fs
@@ -3,9 +3,17 @@ open System.IO
 
 let publicFolder =
     [
+        // If the server is running from src/server
         "../client/public"
+        // If the server is running from src/server/dist
+        "../../client/public"
+        // If the server is running from the dist dir at the root project dir
+        "../src/client/public"
+        // If the server is running from root project dir
         "./src/client/public"
-        "./deploy/public"
+        // If the server is running from root project dir and public folder was copied into dist folder
+        "./dist/public"
+        // If the public folder was copied to wherever the server is running from
         "./public"
     ]
     |> Seq.tryFind Directory.Exists
